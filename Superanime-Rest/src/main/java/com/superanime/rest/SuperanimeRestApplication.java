abc8c29ef90cd4f5a1320f91a46da5b0705e9752
Oskar <oskarf4k3@gmail.com>
2022-12-01 12:18:07 +0100

    Proyecto REST base creado

---
+++ Superanime-Rest/src/main/java/com/superanime/rest/SuperanimeRestApplication.java
@@ -0,0 +1,13 @@
+package com.superanime.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperanimeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperanimeRestApplication.class, args);
+	}
+
+}
