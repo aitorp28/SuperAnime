0f0d93bf1915c61c3af5af4cd92d5b8aaae9324e
Oskar <oskarf4k3@gmail.com>
2022-12-01 12:18:07 +0100

    Proyecto REST base creado

---
+++ Superanime-Rest/src/test/java/com/superanime/rest/SuperanimeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superanime.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperanimeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
