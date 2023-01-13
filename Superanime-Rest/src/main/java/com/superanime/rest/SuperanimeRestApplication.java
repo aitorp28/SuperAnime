c04d16e629a5e3e860d4e9d29455cfd4ed0082bd
Oskar <oskarf4k3@gmail.com>
2023-01-13 12:11:49 +0100

    Documentacion de swagger añadida

--- Superanime-Rest/src/main/java/com/superanime/rest/SuperanimeRestApplication.java
+++ Superanime-Rest/src/main/java/com/superanime/rest/SuperanimeRestApplication.java
@@ -2,8 +2,16 @@ package com.superanime.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 //(scanBasePackages={
 //		"com.superanime.model.dao", "com.superanime.model.entity"})
 public class SuperanimeRestApplication {
@@ -11,5 +19,4 @@ public class SuperanimeRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SuperanimeRestApplication.class, args);
 	}
-
 }
